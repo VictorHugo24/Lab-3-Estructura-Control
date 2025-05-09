6fc0cbf02d9b6ca155fcce71460d8c3685c2941a
VictorHugo24 <vh33079@gmail.com>
2025-05-09 10:14:48 -0600

    Palabra alreves

--- Rodriguez_Victor_Estructura/src/main/java/com/mycompany/rodriguez_victor_estructura/Rodriguez_Victor_Estructura.java
+++ Rodriguez_Victor_Estructura/src/main/java/com/mycompany/rodriguez_victor_estructura/Rodriguez_Victor_Estructura.java
@@ -14,26 +14,72 @@ public class Rodriguez_Victor_Estructura {
 
     public static void main(String[] args) {
         
-        Scanner lea = new Scanner(System.in);
-        int op=0, op1=0, op2=0, op3=0, op4=0;
-        do{
-        System.out.println("****MENU****");
-        System.out.println("1) Palabra alreves");
-        System.out.println("2) Numero Perfecto");
-        System.out.println("3) Primos");
-        System.out.println("4) Votaciones");
-        System.out.println("5) Salir");
-        System.out.print("Por favor ingrese una opcion: ");
-         op = lea.nextInt();
-         
-          if(op>5){
-              System.out.println("Opcion no valida!!!");
-          }
-         
-          switch (op){
-              case 1:
-                  op1++;
-                  break;
+       Scanner lea = new Scanner(System.in);
+        lea.useDelimiter("\n");
+        int op = 0, op1=0, op2=0, op3=0, op4=0;
+        
+        do {
+            System.out.println("****MENU****");
+            System.out.println("1) Palabra alreves");
+            System.out.println("2) Numero Perfecto");
+            System.out.println("3) Primos");
+            System.out.println("4) Votaciones");
+            System.out.println("5) Salir");
+            System.out.print("Por favor ingrese una opcion: ");
+            op = lea.nextInt();
+            
+            if (op > 5) {
+                System.out.println("Opcion no valida!!!");
+            }
+            
+            switch (op) {
+                case 1:
+                    System.out.print("Por favor ingrese la cantidad de palabras: ");
+                    int cantidad = lea.nextInt();
+                    int cont = 0;
+                    String palabraMayor = "";
+                    int maxLength = 0;
+                    while (cont != cantidad) {
+                        System.out.print("Palabra #" + (cont + 1) + ": ");
+                        String palabra = lea.next();
+                        
+                        
+                        String palabraLower = palabra.toLowerCase();
+                        String palabraInvertida = "";
+                        int recorrido = palabra.length() - 1;
+                        
+                        while (recorrido >= 0) {
+                            palabraInvertida += palabra.charAt(recorrido);
+                            recorrido--;
+                        }
+                        System.out.println("Palabra invertida: " + palabraInvertida);
+
+                        if (palabra.length() > maxLength) {
+                            maxLength = palabra.length();
+                            palabraMayor = palabra;  
+                        }
+                        
+                        cont++;
+                    }
+                    System.out.println("La palabra más larga es: " + palabraMayor + " con " + maxLength + " caracteres");
+
+                    String palabraLower = palabraMayor.toLowerCase();
+                    String palabraInvertida = "";
+
+                    int recorrido = palabraMayor.length() - 1;
+                    while (recorrido >= 0) {
+                        palabraInvertida += palabraMayor.charAt(recorrido);
+                        recorrido--;
+                    }
+
+                    if (palabraLower.equals(palabraInvertida.toLowerCase())) {
+                        System.out.println("La palabra mas larga es palindromo");
+                    } else {
+                        System.out.println("La palabra mas larga no es palindromo");
+                    }
+
+                    break; 
+
               case 2:
                   op2++;
                   break;
